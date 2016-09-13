6c04ecd2db85b66ccd9a784b75a6378a60ada896
nicolacavallini <nicola.cavallini@unipv.it>
2016-09-13 12:22:17 +0200

    first commit

---
+++ graph.py
@@ -0,0 +1,21 @@
+# A student's packagies
+
+
+# B student's packagies
+
+
+# B student's function
+
+def eval_func():
+
+    return X,Y,Z
+
+# A student's function
+
+def plot_func(X,Y,Z):
+
+    return
+
+# B student writes the main
+
+if __name__=="__main__":
